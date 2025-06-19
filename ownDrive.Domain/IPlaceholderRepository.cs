1c59bb0148108bd2173aed99be295a3301247df8
k0neru <koneneru@gmail.com>
2025-06-19 23:22:47 +0400

    feat[domain]: declare IPlaceholderRepository and IPlaceholderService

---
+++ ownDrive.Domain/IPlaceholderRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ownDrive.Domain
+{
+	public interface IPlaceholderRepository
+	{
+		public bool Connected { get; }
+
+		public Task Connect();
+		public Task Disconnect();
+		public Task<Stream> DownloadFileAsync(string path, CancellationToken token = default);
+		public Task GetFileListAsync(string subDir, CancellationToken token = default);
+		public Task RemoveAsync(string path, CancellationToken token = default);
+		public Task UploadFileAsync(string path, CancellationToken token);
+	}
+}
---
+++ ownDrive.Domain/IPlaceholderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ownDrive.Domain
+{
+	public interface IPlaceholderService
+	{
+		public Task HydratePlaceholderAsync(string syncRootId, string placeholderPath, CancellationToken token = default);
+
+		public Task DehydratePlaceholderAsync(string syncRootId, string placeholderPath, CancellationToken token = default);
+	}
+}
